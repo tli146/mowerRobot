3af7b81289fa039d4a832e3636064cb78e2e8b24
Tony Li <tli146@hotmail.com>
2023-04-25 20:19:58 +1000

    clustering initial commit

---
+++ mowerRobot_Launch/cluster_laser/scripts/cluster_laser.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+
+#this class will take in lidar msgs and return: list of tree matching obstacles and whether there is an obstacle within collision distance of the robot
+
+from select import select
+import rospy
+import math
+import heapq
+from scipy.spatial.transform import Rotation as R
+import numpy as np
+import modern_robotics as mr
+
+from sensor_msgs import LaserScan
+from std_msgs.msg import Header, String, Int16, Bool, Float32
+
+#constants
+
+#ros constants
+ros_rate = 5
+
+#detected object data
+bollard_diameter = 90/1000 #meter
+
+diameter_max = bollard_diameter*1.5
+diameter_min = bollard_diameter*0.5
+
+obj_link_min_length_wo_distance_adjust = 50/1000 #m
+
+wallLength_max = 0.5 #meter
+
+#safety data
+safety_cone = np.deg2rad(15) #rad
+safety_base = 1 #meter
+
+safety_distance = 2 #meter
+#distance to apply brake
+
+safety_warning = 5 #meter
+#distance to apply slow
+
+lidar_heading_modifier = np.deg2rad(0) #rad
+#lidar heading tuning
+
+
+#degrees
+
+class obstacleList:
+    #takes in laserscan results and generates obstacle list and bollards list
+    def __init__(self, angle_min, angle_max, angle_increment, range_min, range_max, ranges):
+        self.location = [0,0]
+        currentAngle = angle_min
+        x_last_obj = ranges[0]* np.sin(currentAngle)
+        y_last_obj = ranges[0]* np.cos(currentAngle)
+        x_last = x_last_obj
+        y_last = y_last_obj
+        new_obj = True
+
+        obstacle_list = []
+        obstacle_index = 0
+        for laser in ranges:
+            if(laser > range_min and laser < range_max):
+                
+                x = laser* np.sin(currentAngle)
+                y = laser* np.cos(currentAngle)
+
+                if(new_obj):
+                    
+                    obstacle_list.append(obstacle((x,y)))
+                    obstacle_index = obstacle_index + 1
+
+                else:
+
+                    dis_from_last = np.sqrt((x-x_last)^2+(y-y_last)^2)
+                    dis_from_last_obj = np.sqrt((x-x_last_obj)^2+(y-y_last_obj)^2)
+                    linear_scaled_obj_min_length = (0.7+ (laser/15)*0.8)* obj_link_min_length_wo_distance_adjust
+                    #scale by resolution at distance
+
+                    if(dis_from_last < linear_scaled_obj_min_length and dis_from_last_obj < wallLength_max):
+
+                        obstacle_list[obstacle_index].newScan(x, y)
+
+
+                    else:
+
+                        obstacle_list[obstacle_index].finalise()
+
+                        obstacle_list.append(obstacle((x,y)))
+                        obstacle_index = obstacle_index + 1
+
+                new_obj = false
+            else:
+                obstacle_list[obstacle_index].finalise()
+                new_obj = true
+
+        #connect front and back(if connecting)
+        if obstacle_list[1].mergeFrom(obstacle_list[obstacle_index -1]):
+            obstacle_index = obstacle_index - 1
+            obstacle_list.pop(obstacle_index)
+
+    def filter_bollards(self, obstacle_list):
+        None
+
+
+
+
+
+#location
+#      -x
+#       ^
+#       |
+#       |
+#-y <---R-----> y --->angle_min--->front
+#       |
+#       |
+#       x
+#
+
+class obstacle:
+    def __init__(self, x , y) -> None:
+        self.size = 1
+        self.length = 0
+        self.locations_x = []
+        self.locations_y = []
+        self.locations_x.append(x)
+        self.locations_y.append(y)
+        self.x = x
+        self.y = y
+
+    def newScan(self, x, y):
+        self.size = self.size + 1
+        self.locations_x.append(x)
+        self.locations_y.append(y)
+
+    def finalise(self):
+        #find average location
+        self.x = sum(self.locations_x) / len(self.locations_x)
+        self.y = sum(self.locations_y) / len(self.locations_y)
+        self.length = np.sqrt((max(self.locations_x) - min(self.locations_x))^2 + (max(self.locations_y) - min(self.locations_y))^2)
+
+    def mergeFrom(self, obstacle):
+        merged_x = self.locations_x + obstacle.locations_x
+        merged_y = self.locations_y + obstacle.locations_y
+        new_length = np.sqrt((max(merged_x) - min(merged_x))^2 + (max(merged_y) - min(merged_y))^2)
+        if new_length > wallLength_max:
+            return False
+        else:
+            self.size = self.size + obstacle.size
+            self.locations_x = merged_x
+            self.locations_y = merged_y
+            self.x = sum(self.locations_x) / len(self.locations_x)
+            self.y = sum(self.locations_y) / len(self.locations_y)
+            return True
+        
+
+
+
+
+class processor:
+#class of detected obstacles and bollards
+    def detection_callback(self, LaserScan: laserScan):
+        #sync update on receiving new transform information
+        self.laserScan = laserScan
+        obstacleList = obstacleList(LaserScan.angle_min, LaserScan.angle_max, LaserScan.angle_increment, LaserScan.range_min, LaserScan.range_max, LaserScan.ranges)
+
+    
+
+    def __init__(self):
+
+
+        self.sub_laserScan= rospy.Subscriber(
+            #subscribe to lidar output
+        "LaserScan",
+        laserScan,
+        self.detection_callback       
+        )
+
+
+    
+            
+
+
+
+if __name__ == '__main__':
+
+    rospy.init_node('cluster_laser')
+
+    #set frequency to increase performance
+    rate = rospy.Rate(ros_rate)
+    scan_processor = processor()
+    #instantiate object
+    
+    
+        
+    
+    while not rospy.is_shutdown():
+        
+        rate.sleep()
+
+
